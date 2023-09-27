dff9b442d14ce8b7c1c136a6cb5d6b8d399ba498
lino62U <alupoc@unsa.edu.pe>
2023-09-27 12:08:15 -0500

    Initial commit

---
+++ src/main/java/com/project/eleccionOnline/EleccionOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.project.eleccionOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleccionOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleccionOnlineApplication.class, args);
+	}
+
+}
